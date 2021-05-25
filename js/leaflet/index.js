e09f463043bdb72362b94ea79964188b46598462
David Molineus <david.molineus@netzmacht.de>
2021-05-25 14:00:36 +0200

    Images are provided without default with latest webpack version

--- js/leaflet/index.js
+++ js/leaflet/index.js
@@ -6,9 +6,9 @@ import { ATTRIBUTION} from "../config";
 delete leaflet.Icon.Default.prototype._getIconUrl;
 
 leaflet.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
-    iconUrl: require('leaflet/dist/images/marker-icon.png').default,
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
 });
 
 leaflet.Control.Attribution.addInitHook(function() {
